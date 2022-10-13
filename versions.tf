ec57c7776510c9e0e636fda15c578a7cbfa4a26d
Igor Rodionov <goruha@gmail.com>
2022-10-13 21:17:37 +0300

    Update version constrains

--- versions.tf
+++ versions.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "> 4.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "> 2.2"
+    }
     # Update these to reflect the actual requirements of your module
     random = {
       source  = "hashicorp/random"
