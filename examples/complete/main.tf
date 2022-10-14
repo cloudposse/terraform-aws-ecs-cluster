033ebdfb1307ef87b8cd7751a45bc7ad4803209c
Igor Rodionov <goruha@gmail.com>
2022-10-14 03:20:28 +0300

    Update version constrains

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,8 +17,8 @@ module "subnets" {
   stage                = var.stage
   name                 = var.name
   vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
   nat_gateway_enabled  = false
   nat_instance_enabled = false
 }
