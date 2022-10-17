c85fbde4bfe0fe9b546368de8694c7183e20e52f
Igor Rodionov <goruha@gmail.com>
2022-10-17 21:32:11 +0300

    Use options default values

--- variables.tf
+++ variables.tf
@@ -104,7 +104,9 @@ variable "capacity_providers_ec2" {
         spot_instance_pools                      = number
         spot_max_price                           = string
       })
-    }))
+    }), {
+      instances_distribution = null
+    })
     placement = optional(object({
       affinity          = string
       availability_zone = string
