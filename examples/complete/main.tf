6b0170f3fd3577aa7d2fb507a409763f13afdf96
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-11-18 16:27:55 +0000

    Auto Format

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -40,15 +40,15 @@ module "ecs_cluster" {
 }
 
 data "aws_ssm_parameter" "ami" {
-  name  = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
 module "autoscale_group" {
-  source = "cloudposse/ec2-autoscale-group/aws"
+  source  = "cloudposse/ec2-autoscale-group/aws"
   version = "0.31.1"
 
-  name        = "external-capacity-provider"
-  context     = module.this.context
+  name    = "external-capacity-provider"
+  context = module.this.context
 
   image_id                    = data.aws_ssm_parameter.ami.value
   instance_type               = "t3.medium"
