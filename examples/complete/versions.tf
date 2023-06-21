f0f04a71ee0c23471ae0ead0387f06e23a0b69a8
Max Lobur <max_lobur@outlook.com>
2023-06-21 13:53:24 +0300

    update tests

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.3"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.1.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
   }
 }
