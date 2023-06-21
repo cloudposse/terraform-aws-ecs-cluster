d1c23ea9f56902274ee832c9f8500703dd7e3077
Max Lobur <max_lobur@outlook.com>
2023-06-21 13:30:39 +0300

    updates

--- ec2.tf
+++ ec2.tf
@@ -41,7 +41,7 @@ module "autoscale_group" {
   for_each = local.ec2_capacity_providers
 
   source  = "cloudposse/ec2-autoscale-group/aws"
-  version = "0.31.1"
+  version = "0.34.2"
 
   context = module.ecs_labels[each.key].context
 
