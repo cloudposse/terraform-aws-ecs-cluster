0e5d549d8ba140501ce3a8da18e047defa2a7bfe
Carlo Lucadei <clucadei@kseniasecurity.com>
2024-09-03 18:51:18 +0200

    Add missing description for region and availability_zones

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,7 +1,9 @@
 variable "region" {
   type = string
+  description = "AWS region"
 }
 
 variable "availability_zones" {
   type = list(string)
+  description = "List of Availability Zones IDs (e.g. `['us-east-1a', 'us-east-1b', 'us-east-1c']`)"
 }
