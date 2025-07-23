2e7c5132eebed16565d87513cf63aa9e7c8ad0e0
Jan Costandius <jcostandius@gmail.com>
2025-07-23 19:15:00 +0200

    Provider v6 Compatibility - Remove `elastic_gpu_specifications`. (#67)
    * fix: remove deprecated elastic_gpu_specifications
    
    - bumped `ec2-autoscale-group` to `0.41.1`
    
    * fix: Update autoscale group module to version 0.41.1
    
    * fix: Update dynamic-subnets module to version 2.4.2
    
    This fixes the error where the VPC parameter was passed to EIP, which is
    likely deprecated.
    
    * fix: Bump AWS provider version to >= 6.0
    
    * Update versions.tf
    
    Co-authored-by: RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
    
    * fix: AWS provider version constraint syntax
    
    ---------
    
    Co-authored-by: RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 
 module "subnets" {
   source               = "cloudposse/dynamic-subnets/aws"
-  version              = "2.3.0"
+  version              = "2.4.2"
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
   igw_id               = [module.vpc.igw_id]
@@ -67,7 +67,7 @@ data "aws_ssm_parameter" "ami" {
 module "autoscale_group" {
   count   = var.enabled ? 1 : 0
   source  = "cloudposse/ec2-autoscale-group/aws"
-  version = "0.34.2"
+  version = "0.41.1"
 
   context = module.this.context
 
